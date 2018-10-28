c4226f969131bff73e2bb24f489f1e21ec31ff77
farhana0074 <sp15bs0074@maju.edu.pk>
2018-10-28 13:51:28 -0700

    Main fragments for HodMainActivity and FacultyMainActivity, created

---
+++ app/src/main/java/com/farhakhan/majulams/Faculty_Main.java
@@ -0,0 +1,32 @@
+package com.farhakhan.majulams;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+public class Faculty_Main extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Please note the third parameter should be false, otherwise a java.lang.IllegalStateException maybe thrown.
+        View view = inflater.inflate(R.layout.fragment_faculty_main, container, false);
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM, yyyy ");
+        String strDate = dateFormat.format(calendar.getTime());
+        TextView txt_date= view.findViewById(R.id.Admin_Date);
+        txt_date.setText(strDate);
+        SimpleDateFormat day = new SimpleDateFormat("EEEE");
+        String strDay=day.format(calendar.getTime());
+        TextView txt_day = view.findViewById(R.id.Admin_Day);
+        txt_day.setText(strDay);
+        return view;
+    }
+}
